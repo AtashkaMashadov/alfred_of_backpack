04921abce092a3c8e0dad80ab3e1d3ed4f9362a0
Atadurdy Mashadov <113720165+AtashkaMashadov@users.noreply.github.com>
2022-09-17 00:15:24 +0500

    Add files via upload

---
+++ WeightOfTheBag/Alfred_of_backpack.py
@@ -0,0 +1,28 @@
+print("how many lessons doy you have tomorrow?")
+a = int(input())
+print("it means that you will carry",a,"books")
+input("""\n\npress enter, if that is true
+if not restart the program and change the number of lessons""")
+print("How much weight does 1 book has?")
+b = float(input())
+print("OK,mmmmm, A! yes and how much weight does your bag has")
+c = float(input())
+print(" - - - - - - - - - - - -")
+print("OK, lets start, you have",a,"lessons and",a,"books")
+print("and the weight of one book is",b,)
+print("Just a second,......AHA!")
+print("Ive just calculated and the weight of your bag is",b*a+c,)
+print("That is cool!")
+print('You can every day count a weight of your bag!')
+print("And now would it be heavy for you or someone else!")
+print(" - - - - - - - - - - - - - - - - - - - - - - - - - -")
+print("""
+=============
+|           |
+|    BYE!   |
+|           |
+=============
+""")
+print("""                 Information about the program
+written in:PYTHON (programming language)
+written by:Atadurdy Mashadow""")
